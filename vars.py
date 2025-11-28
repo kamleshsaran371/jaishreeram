ac57ed3f51e983b36d2a58cf0a80c99c25eb7115
kamleshsaran371 <kamleshsaran371@gmail.com>
2025-11-28 11:56:43 +0530

    Change BOT_TOKEN to new environment variable
    Updated BOT_TOKEN to a new value.

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29899250"))
 API_HASH = environ.get("API_HASH", "611d045796c79af3e5ddfa3d6fd536a7")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8537572918:AAG7RDp5j1sai9v2n8XXsoRECRZMCvrYia8")
 
 OWNER = int(environ.get("OWNER", "6966002582"))
 CREDIT = environ.get("CREDIT", "@kmxretro")
